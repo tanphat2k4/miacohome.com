88d0664b100aadb8cd69270be5db06a3031a48c4
administrator <administrator@DOMAINPC>
2018-05-19 21:06:43 +0700

    Add project files.

---
+++ miacohome.com/Controllers/cONTACTSController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace miacohome.com.Controllers
+{
+    public class cONTACTSController : Controller
+    {
+        // GET: cONTACTS
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
