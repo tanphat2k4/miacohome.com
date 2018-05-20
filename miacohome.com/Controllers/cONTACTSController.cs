7c2d966f49ca26cbc280b5f08d08c08f55538c1f
administrator <administrator@DOMAINPC>
2018-05-20 12:00:56 +0700

    send mail with Gmail

--- miacohome.com/Controllers/cONTACTSController.cs
+++ miacohome.com/Controllers/cONTACTSController.cs
@@ -15,46 +15,26 @@ namespace miacohome.com.Controllers
     public class cONTACTSController : RenderMvcController
     {
         // GET: cONTACTS
-        public ActionResult ContactsPage(RenderModel model)
+        public ActionResult ContactsPage(ContactModel model)
         {
             if (Request.HttpMethod == "POST")
             {
-                var fullName = Request["name"];
-                var Email = Request["email"];
-                var subject = Request["subject"];
-                var message = Request["message"];
-                var dateNow = string.Format("[0]-[1]", DateTime.Now.ToString());
+                
                 //Creat Service
                 var contentServices = Services.ContentService;
-                var person = contentServices.CreateContent(fullName, model.Content.Id, "potential_Customer");
-                //SendMail(model);
+                var person = contentServices.CreateContent(model.name, CurrentPage.Id, "potential_Customer");
                 //Creat Person
-                person.SetValue("name_customer", fullName);
-                person.SetValue("email_customer", Email);
-                person.SetValue("subject", subject);
-                person.SetValue("message", message);
-                
+                person.SetValue("name_customer", model.name);
+                person.SetValue("email_customer", model.email);
+                person.SetValue("subject", model.subject);
+                person.SetValue("message", model.message);
+                SendMail(model);
                 //refesh page
                 contentServices.SaveAndPublishWithStatus(person);
             }
             return CurrentTemplate(model);
         }
-        //public ActionResult HandleSubmitForm(ContactModel model)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        var contentServices = Services.ContentService;
-        //        var message = Services.ContentService.CreateContent(String.Format("{0}-{1}", model.name, DateTime.Now.ToString()), CurrentPage.Id, "potential_Customer");
-        //        message.SetValue("name_customer", model.name);
-        //        message.SetValue("email_customer", model.email);
-        //        message.SetValue("message", model.message);
-        //        message.SetValue("subject", model.subject);
-        //        //refesh page
-        //        contentServices.SaveAndPublishWithStatus(message);
-      
-        //    }
-        //    return CurrentTemplate(model);
-        //}
+       
         private void SendMail(ContactModel model)
         {
             var formEmail = new MailAddress(System.Configuration.ConfigurationManager.AppSettings["sendEmailForm"]);
